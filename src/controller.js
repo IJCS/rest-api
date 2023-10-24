d45b11b9404d3358e4b1e454affc19304c2f325a
IJCS <ijcs123@gmail.com>
2023-10-24 10:20:22 -0300

    ID Check Exceptions

--- src/controller.js
+++ src/controller.js
@@ -9,10 +9,14 @@ class LibrosController{
   async getOne(req, res){
     try {
       const carga = req.body;
+      if(!carga.id){
+        throw new Error("ID Requerido");
+      }
       const [result] = await pool.query(`SELECT * FROM libros WHERE id=(?)`,[carga.id]);
       res.json(result);
     } catch (e) {
-      res.json({"Error":e});
+      res.json({"Error":e.message});
+      console.log("Error:" + e.message);
     }
   }
 
@@ -20,38 +24,44 @@ class LibrosController{
     try {
       const carga = req.body;
 
-      if (typeof carga !== 'object' || Array.isArray(carga)) {
-        throw new Error("JSON no obtenido");
-      }
-
       if(!carga.nombre || !carga.autor || !carga.categoria || !carga.año || !carga.ISBN){
         throw new Error("Datos Requeridos");
       }
       const [result] = await pool.query(`INSERT INTO libros (nombre , autor , categoria , \`año-publicacion\` , ISBN ) VALUES (?, ?, ?, ?, ?)`,[carga.nombre,carga.autor,carga.categoria,carga.año,carga.ISBN]);
       res.json({"ID Insertado":result.insertId});
     } catch (e) {
-      console.log(e);
+      res.json({"Error":e.message});
+      console.log("Error:" + e.message);
     }
   }
 
   async remove(req, res){
-    try
-    {
+    try{
       const carga = req.body;
+      if(!carga.id){
+        throw new Error("ID Requerido");
+      }
       const [result] = await pool.query(`DELETE FROM libros WHERE id =(?)`,[carga.id]);
       res.json({"Registros eliminados":result.affectedRows});
-    }
-    catch (e)
-    {
-      res.json({"Error":e});
+    } catch (e) {
+      res.json({"Error":e.message});
+      console.log("Error:" + e.message);
     }
 
   }
 
   async modify(req, res){
-    const carga = req.body;
-    const [result] = await pool.query(`UPDATE libros SET nombre = (?), autor=(?), categoria=(?), \`año-publicacion\` = (?), ISBN= (?) WHERE id = (?)`, [carga.nombre,carga.autor,carga.categoria,carga.año,carga.ISBN,carga.id]);
-    res.json({"Registros modificados":result.affectedRows});
+    try {
+      const carga = req.body;
+      if(!carga.id){
+        throw new Error("ID Requerido");
+      }
+      const [result] = await pool.query(`UPDATE libros SET nombre = (?), autor=(?), categoria=(?), \`año-publicacion\` = (?), ISBN= (?) WHERE id = (?)`, [carga.nombre,carga.autor,carga.categoria,carga.año,carga.ISBN,carga.id]);
+      res.json({"Registros modificados":result.affectedRows});
+    } catch (e) {
+      res.json({"Error":e.message});
+      console.log("Error:" + e.message);
+    }
   }
 
   async getISBN(req,res){
